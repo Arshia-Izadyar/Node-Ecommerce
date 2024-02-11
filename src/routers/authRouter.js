26cdca2064a9612a376d0a9e99c83ce689b7cb62
Rshia Izadyar <arshiaa104@gmail.com>
2024-02-11 13:46:18 +0330

    Feature: user address/fullname update & profile

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,22 +1,26 @@
-const express = require("express");
+const express = require('express')
 
 const {
-  register,
-  login,
-  verifyUser,
-  requestVerification,
-  logout,
-  refresh,
-} = require("../controllers/authController");
-const { authenticate, authorization } = require("../middlewares/index");
+    register,
+    login,
+    verifyUser,
+    requestVerification,
+    logout,
+    refresh,
+    profile,
+    updateProfile,
+} = require('../controllers/authController')
+const { authenticate, authorization } = require('../middlewares/index')
 
-const router = express.Router();
+const router = express.Router()
 
-router.route("/register").post(register);
-router.route("/verify").post(verifyUser);
-router.route("/login").post(login);
-router.route("/logout").post(authenticate, logout);
-router.route("/refresh").get(refresh);
-router.route("/request-verify").post(requestVerification);
+router.route('/register').post(register)
+router.route('/verify').post(verifyUser)
+router.route('/login').post(login)
+router.route('/logout').post(authenticate, logout)
+router.route('/refresh').get(refresh)
+router.route('/profile').get(authenticate, profile)
+router.route('/profile/update').patch(authenticate, updateProfile)
+router.route('/request-verify').post(requestVerification)
 
-module.exports = router;
+module.exports = router
